3e79f496c2a15ce4fbdfac3d1381161d20189f05
Jose Bieco <jo_bieco@hotmail.com>
2021-08-19 17:19:26 -0300

    refactor(HoleriteProps): new state

--- src/webparts/holerite/components/HoleriteProps.ts
+++ src/webparts/holerite/components/HoleriteProps.ts
@@ -1,44 +1,7 @@
-interface Version {
-  name: string;
-  url: string;
-}
-
-export interface Game {
-  game_index: number;
-  version: Version;
-}
-
-export interface HoleriteProps {
-  name: string;
-  order: number;
-  weight: number;
-  img: string;
-  games_in: Game[];
-}
-
-export const HoleriteInitialProps:HoleriteProps = {
-  name: '',
-  order: 0,
-  weight: 0,
-  img: '',
-  games_in: [],
-}
-
-
 export interface HoleriteState {
-  name: string;
-  order: number;
-  weight: number;
-  img: string;
-  games_in: Game[];
-  isOpen: boolean;
+  employee_code: string;
 }
 
 export const HOLERITE_INITIAL_STATE: HoleriteState = {
-  name: '',
-  order: 0,
-  weight: 0,
-  img: '',
-  games_in: [],
-  isOpen: false,
+  employee_code: '',
 }
