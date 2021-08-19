9d29b71e934f34d0c64ffa3bdd8890af14cd77d3
Jose Bieco <jo_bieco@hotmail.com>
2021-08-19 17:18:13 -0300

    refactor(Holerite): create new components

--- src/webparts/holerite/components/Holerite.tsx
+++ src/webparts/holerite/components/Holerite.tsx
@@ -1,85 +1,35 @@
 import * as React from 'react';
 import axios from 'axios';
-import styles from './Holerite.module.scss';
-import { HoleriteProps, HoleriteInitialProps, HoleriteState, Game, HOLERITE_INITIAL_STATE } from './HoleriteProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { HoleriteWebPartProps } from '../HoleriteWebPart';
-import Display from './Display';
-import Modal from './Modal';
+import { useState, KeyboardEvent } from 'react';
+import { HoleriteState, HOLERITE_INITIAL_STATE } from './HoleriteProps';
+import { Grid } from '@material-ui/core';
+import SearchButton from './SearchButton';
+import HoleriteDisplay from './HoleriteDisplay';
 
-export default class Holerite extends React.Component<HoleriteWebPartProps, HoleriteState> {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
 
-  public constructor(props: HoleriteWebPartProps,state: HoleriteState){
-    super(props);
-    this.state = HOLERITE_INITIAL_STATE;
-    this.setState(HOLERITE_INITIAL_STATE);
-  }
+  }),
+);
 
-  public async componentDidMount() {
-    try{
-      this.setState({games_in: []});
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
-      let newHolerite: HoleriteProps = HoleriteInitialProps;
+const Holerite: React.FC<HoleriteWebPartProps> = ({description}, HoleriteWebPartProps) => {
+  const classes = useStyles();
+  const [state, setState] = useState<HoleriteState>(HOLERITE_INITIAL_STATE);
 
-      newHolerite.name = response.data.name;
-      newHolerite.weight = response.data.weight;
-      newHolerite.order = response.data.order;
-      newHolerite.img = response.data.sprites.front_shiny ? response.data.sprites.front_shiny : response.data.sprites.front_default;
-
-      if(!this.state.games_in.length){
-        response.data.game_indices.map((game: Game) => {
-          newHolerite.games_in.push(game);
-        });
-      }
-
-      this.setState(newHolerite);
-    }catch(error){
-      console.log(error);
-    }
-  }
-
-  public render(): React.ReactElement<HoleriteProps> {
-    return (
-      <div className={ styles.holerite }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <a className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-            <div>
-              <p>{`Name: ${this.state.name}`}</p>
-              <p>{`Weigth: ${this.state.weight}`}</p>
-              <p>{`Oredr: ${this.state.order}`}</p>
-              <img src={this.state.img} />
-            </div>
-          </div>
-        </div>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}>
-          {
-            this.state.games_in.map(game => (
-             <Display game={game} />
-            ))
-          }
-          </div>
-          <button onClick={() => {this.setState({isOpen: !this.state.isOpen})}}>OPEN</button>
-          {this.state.isOpen &&
-            <Modal isOpen={this.state.isOpen}/>
-          }
-      </div>
-    );
-  }
-}
-
-// <p className={ styles.description }>{escape(this.props.description)}</p>
-
-/* const Holerite: React.FC<HoleriteWebPartProps> = ({description}, HoleriteWebPartProps) => {
   return(
-    <div>{description}</div>
+    <div className={classes.root}>
+       <Grid container spacing={3}>
+         <SearchButton state={state} setState={setState} />
+        <Grid item xs={12}>
+          <HoleriteDisplay />
+        </Grid>
+      </Grid>
+    </div>
   )
 }
-  export default Holerite;
-*/
+export default Holerite;
