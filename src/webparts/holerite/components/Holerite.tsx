db6674319ec40ee0be87abad55bbbb35f8871841
Jose Bieco <jo_bieco@hotmail.com>
2021-08-31 16:18:11 -0300

    refactor(Holerite): refactored all the render logic, implemented the request to api and user id validation

--- src/webparts/holerite/components/Holerite.tsx
+++ src/webparts/holerite/components/Holerite.tsx
@@ -1,36 +1,158 @@
 import * as React from 'react';
-import axios from 'axios';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { HoleriteWebPartProps } from '../HoleriteWebPart';
-import { useState, KeyboardEvent } from 'react';
-import { HoleriteState, HOLERITE_INITIAL_STATE } from './HoleriteProps';
-import { Grid } from '@material-ui/core';
+import { useState } from 'react';
+import { Error, Holerite, HoleriteState, HOLERITE_INITIAL_STATE } from './HoleriteProps';
+import { Paper, Grid, Typography } from '@material-ui/core';
 import SearchButton from './SearchButton';
 import HoleriteDisplay from './HoleriteDisplay';
+import { Api } from '../../../index';
+import ErrorDisplay from './HoleriteDisplayParts/ErrorDisplay';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      height: 200,
     },
-
+    expandedPage: {
+      height: 600,
+    },
+    button: {
+      height: 300,
+    }
   }),
 );
 
-const Holerite: React.FC<HoleriteWebPartProps> = ({description, context}, HoleriteWebPartProps) => {
+const Holerite: React.FC<HoleriteWebPartProps> = ({description, context, properties}: HoleriteWebPartProps) => {
   const classes = useStyles();
-  const [state, setState] = useState<HoleriteState>(HOLERITE_INITIAL_STATE);
+  const api = new Api(properties.api_user , properties.api_password , properties.api_url);
 
-  console.log(context);
+  const [state, setState] = useState<HoleriteState>(HOLERITE_INITIAL_STATE);
+  const [loadPage, setLoadPage] = useState(false);
+  const [fail, setFail] = useState(false);
+  const [file, setFile] = useState('');
+  const [monthCode, setMonthCode] = useState('');
+
+  // TODO: Create a function that recives a message and set it on state.error
+  /**
+   * Receive the user id and validates if the given string has only numbers
+   * @param code String
+   * @returns Boolean
+   */
+  const validateUserCode = (code: string): boolean => {
+    let regex: RegExp = /[^0-9]/g;
+
+    let validatedCode: string = code.replace(regex, '');
+
+    return !validatedCode || validatedCode.length !== code.length;
+  };
+
+  /**
+   * Load all the Payslip list from the logged user
+   */
+  const getHoleriteList =  async () => {
+    try {
+      let user_id = context.pageContext.user.loginName.split('@')[0];
+
+      // If the login do not contain a valid user code, a message will be set in state.error
+      if(validateUserCode(user_id)) {
+        let newErro: Error = {message: 'Código inválido!'};
+        setState({...state, error: [...state.error, newErro]});
+        setFail(true);
+        return;
+      }
+
+      setState({...state, user_id: user_id});
+
+      const response = await api.connection.get(`/ListaHolerite/11111111111/4001/${user_id}`);
+
+      if(response.status === 200) {
+        // If the response returns any kind of error
+        if(response.data.MT_Holerite_RES.ERP.Mensagem){
+          let newErro: Error = {message: response.data.MT_Holerite_RES.ERP.Mensagem};
+          setState({...state, error: [...state.error, newErro]});
+          setFail(true);
+          return;
+        }
+
+        // If the user don't have any payslip, set a message and not load the Search Button component
+        if(!response.data.MT_Holerite_RES.ERP.Content){
+          let newErro: Error = {message: 'O colaborador não possui nenhum holerite cadastrado!'};
+          setState({...state, error: [...state.error, newErro]});
+          setFail(true);
+          return;
+        }
+
+        const list: Holerite[] = [];
+        let reversedArray = response.data.MT_Holerite_RES.ERP.Content.reverse();
+        for(let i = 0; i < reversedArray.length && i < 12; i++){
+          let item = reversedArray[i];
+          list.push({CODIGO: item.CODIGO, DESCRICAO: item.DESCRICAO, IPERI: item.IPERI});
+        }
+
+        setState({...state, holerites: list});
+      }
+    }catch(err) {
+      let newErro: Error = {message: 'Ocorreu um erro ao carregar suas informações!'};
+      setFail(true);
+      setState({user_id: state.user_id, month_code: state.month_code, holerites: state.holerites, isLoading: false, error: [...state.error, newErro]});
+    }
+  };
+
+  React.useEffect(() => {
+    getHoleriteList();
+  }, []);
+
+  /**
+   * Load the specific payslip
+   * @returns
+   */
+  const getHolerite = async (): Promise<void> => {
+    try{
+      let user_id = context.pageContext.user.loginName.split('@')[0];
+      const response = await api.connection.get(`/Holerite/4001/${monthCode}/${monthCode}/11111111111/${user_id}`);
+
+      if(response.status === 200) {
+        if(response.data.MT_Holerite_RES.ERP.Mensagem){
+          let newErro: Error = {message: response.data.MT_Holerite_RES.ERP.Mensagem};
+          setState({...state, error: [...state.error, newErro]});
+          setFail(true);
+        }else {
+          setFile(response.data.MT_Holerite_RES.ERP.ContentBin);
+          setLoadPage(true);
+        }
+      }
+    }catch(err) {
+      let newErro: Error = {message: 'Ocorreu um erro ao tentar exibir o seu holerite!'};
+      setState({...state, error: [...state.error, newErro]});
+      setFail(true);
+    }
+  };
+
   return(
-    <div className={classes.root}>
-       <Grid container spacing={3}>
-         <SearchButton state={state} setState={setState} />
-          <Grid item xs={12}>
-            <HoleriteDisplay />
+    <div className={`${classes.root} ${(loadPage && !fail) ? classes.expandedPage : ''} ${ !fail ? (loadPage ? classes.expandedPage : classes.button) : ''}`}>
+      <Typography variant="body1" align="center" gutterBottom style={{ fontWeight: 'bold', color: '#252627', fontSize: '1.5em' }}>
+        Demonstrativo de Pagamento
+      </Typography>
+      <Grid container spacing={2}>
+        {state.error.length !== 0 &&
+          <ErrorDisplay errors={state.error} />
+        }
+        {(!state.error.length) &&
+          <SearchButton
+          state={state}
+          setMonthCode={setMonthCode}
+          getHolerite={getHolerite}
+          setLoadPage={() => setLoadPage(true)} />
+        }
+        {(loadPage && !fail) &&
+          <Grid item xs={12} justifyContent="center">
+            <HoleriteDisplay file={file}/>
           </Grid>
+        }
       </Grid>
     </div>
-  )
-}
+  );
+};
 export default Holerite;
