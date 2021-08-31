ccab1595b658bc0afde0aed7ce35df3202b740b2
Jose Bieco <jo_bieco@hotmail.com>
2021-08-31 16:07:37 -0300

    feat(HoleriteProps.ts): all interfaces and state initial values

--- src/webparts/holerite/components/HoleriteProps.ts
+++ src/webparts/holerite/components/HoleriteProps.ts
@@ -1,7 +1,29 @@
 export interface HoleriteState {
-  employee_code: string;
+  user_id: string;
+  month_code: string;
+  holerites: Array<Holerite>;
+  isLoading: boolean;
+  error: Array<Error>;
 }
 
 export const HOLERITE_INITIAL_STATE: HoleriteState = {
-  employee_code: '',
+  user_id: '',
+  month_code: '',
+  holerites: [],
+  isLoading: false,
+  error: []
+};
+
+/**
+ * Interface that represents the response single data
+ */
+export interface Holerite {
+  CODIGO: string;
+  DESCRICAO: string;
+  IPERI: string;
+  file?: Blob;
+}
+
+export interface Error {
+  message: string;
 }
