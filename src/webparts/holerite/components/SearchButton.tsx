d597292b625ec4be9f113ced748f04231018e892
Jose Bieco <jo_bieco@hotmail.com>
2021-08-31 16:13:42 -0300

    refactor(SearchButton): change the logic and includes the holerite array

--- src/webparts/holerite/components/SearchButton.tsx
+++ src/webparts/holerite/components/SearchButton.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { Button, TextField, Grid, Paper, ButtonGroup } from '@material-ui/core';
+import { Button, TextField, Grid, Paper, ButtonGroup, Tooltip } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import AutorenewIcon from '@material-ui/icons/Autorenew';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import { HoleriteState } from './HoleriteProps';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -15,97 +20,97 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     holerite: {
       justifyContent: 'cenetr',
-    }
+    },
+    formControl: {
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
   }),
 );
 
 export interface SearchButtonProps {
   state: HoleriteState;
-  setState: React.Dispatch<React.SetStateAction<HoleriteState>>;
+  setMonthCode: React.Dispatch<React.SetStateAction<string>>;
+  setLoadPage: () => void;
+  getHolerite: () => void;
 }
 
-const SearchButton: React.FC<SearchButtonProps> = ({ state, setState }, SearchButtonProps) => {
+const SearchButton: React.FC<SearchButtonProps> = ({ state, setMonthCode, setLoadPage, getHolerite }: SearchButtonProps) => {
   const classes = useStyles();
   const [code, setCode] = useState('');
-  const [validCode, setValidCode] = useState(true);
-  const [loadingData, setLoadingData] = useState(false);
 
-  const handleCodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleCodeChange = (event) => {
     setCode(event.target.value);
+    setMonthCode(event.target.value);
+  };
+
+  const onSubmit = (): void => {
+    getHolerite();
   };
 
   const handleKeyPress = (event): void => {
-    console.log(event.key)
     if(event.key === 'Enter'){
-      validateEntry();
+      onSubmit();
     }
-  }
+  };
 
-  const validateEntry = (): void => {
-    let regex: RegExp = /[^\d^a-z^A-Z]/g;
+  /**
+   * Get unformated data (yyyymm) and retuns it on mm/yyyy format
+   * @param data String
+   * @returns String
+   */
+  const dataFormat = (data: string): string => {
+    return data.toString().slice(4) + '/' + data.toString().slice(0,4);
+  };
 
-    let validatedCode: string = code.replace(regex, '');
+  /**
+   * From state.holerites, render the menu items
+   * @returns Array of JSX Element
+   */
+  const renderMenuItems = () => {
+    const itemsToRender = [];
 
-    if(!validatedCode || validatedCode.length !== code.length){
-      setValidCode(false);
-    }else {
-      setValidCode(true);
-      setLoadingData(true);
+    for(let i = 0; i < state.holerites.length; i++){
+      const item = state.holerites[i];
+      itemsToRender.push(
+        <MenuItem value={item.CODIGO}>{dataFormat(item.IPERI)}</MenuItem>
+      );
     }
-  }
-
-  const onSubmit = (): void => {
-    validateEntry();
-
-    if(validCode){
-      setState({employee_code: code});
-      setCode('');
-    }
-  }
+    return itemsToRender;
+  };
 
   return(
     <Grid item xs={12} onKeyPress={handleKeyPress}>
       <Paper className={classes.paper} >
         <ButtonGroup variant="contained" style={{justifyContent: 'center'}}>
-          {validCode &&
-          <>
-            <TextField
-              id="employee"
-              label="Código"
-              variant="outlined"
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="select-label-data">Data</InputLabel>
+            <Select
+              labelId="select-label-data"
+              id="select-label-data"
               value={code}
               onChange={handleCodeChange}
-            />
+              label="Data">
+              {renderMenuItems()}
+            </Select>
+          </FormControl>
 
+          <Tooltip title="Pesquisar">
             <Button variant="contained" color="primary" onClick={onSubmit} >
-              {loadingData &&
+              {state.isLoading &&
                 <AutorenewIcon />
               }
-              {!loadingData &&
+              {!state.isLoading &&
                 <SearchIcon />
               }
             </Button>
-          </>
-          }
-          {!validCode &&
-          <>
-            <TextField
-              error
-              id="employee"
-              label="Erro Código"
-              variant="outlined"
-              value={code}
-              onChange={handleCodeChange}
-            />
-            <Button variant="contained" color="secondary" onClick={onSubmit} >
-              <SearchIcon />
-            </Button>
-          </>
-          }
+          </Tooltip>
         </ButtonGroup>
       </Paper>
     </Grid>
   );
-}
+};
 
 export default SearchButton;
